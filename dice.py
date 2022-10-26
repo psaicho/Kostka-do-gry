ceb1a59d1f96e09e61a1e1115184ae33b021b001
psaicho <guzekt@gmail.com>
2022-10-26 15:34:58 +0200

    zrealizowano v1

--- dice.py
+++ dice.py
@@ -4,18 +4,20 @@ import random
 
 
 def sum_of_random_moves(type_of_dice: int, move: int) -> int:
-    return sum(random.randint(1, type_of_dice+1) for i in range(move))
+    return sum(random.randint(1, type_of_dice + 1) for i in range(move))
+
+
+def check_text(text:str) -> bool:
+    try:
+        text.split()
+        return True
+    except (AttributeError, TypeError):
+        return False
 
 
 def roll_the_dice(text: str) -> list:
     patern = r"[0-9]*[D](3|4|6|8|10|12|20|100){1}((\+|\-)?[0-9]*)?"
-    try:
-        text.split()
-        re.fullmatch(r"\s", text)
-    except (AttributeError, TypeError):
-        result = "The specified string is not code for roll of dice"
-        pass
-    if re.fullmatch(patern, text) is None:
+    if check_text(text) == False or re.fullmatch(patern, text) is None:
         result = "The specified string is not code for roll of dice"
     else:
         dice = re.split("[D | \+ | \-]", text)
@@ -25,11 +27,11 @@ def roll_the_dice(text: str) -> list:
         if len(dice) == 2:
             result = sum_of_random_moves(dice[1], dice[0])
         else:
-            opr = re.findall("[\+ | \-]", text)[0]
             operators = {
                 '+': operator.add,
                 '-': operator.sub
             }
+            opr = re.findall("[\+ | \-]", text)[0]
             result = operators.get(opr)(sum_of_random_moves(dice[1], dice[0]), dice[2])
 
     return result
@@ -43,10 +45,3 @@ if __name__ == "__main__":
     print(roll_the_dice("DD34"))
     print(roll_the_dice("4-3D6"))
     print(roll_the_dice(1))
-
-"""
-2D10+10 
-D6
-2D3
-D12-1
-"""
